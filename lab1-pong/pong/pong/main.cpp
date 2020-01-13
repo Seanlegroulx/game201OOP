2a992d52ba75a30f314a4065538d1f4908c0a62c
Sean Legroulx [Student] <sean.legroulx@student.sl.on.ca>
2020-01-13 14:16:35 -0500

    added pong VScode part0.5

--- lab1-pong/pong/pong/main.cpp
+++ lab1-pong/pong/pong/main.cpp
@@ -1,7 +1,62 @@
 /* 
 Sean Legroulx
 Game 202
-Majid 
+Majid Moghadam
+Pong
+1/12/2020
 
 
 */
+
+#include "sdl.h"
+
+#define WINDOW_WIDTH  800
+#define WINDOW_HIGHT  600
+
+
+SDL_Renderer *renderer;
+
+bool running = true;
+
+
+SDL_Rect playerpaddle;
+SDL_Rect AIpaddle;
+SDL_Rect Ball;
+
+void LoadGame()
+{
+	SDL_Window *window;
+
+	window = SDL_CreateWindow("Basic SDL Sprites",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WINDOW_WIDTH, WINDOW_HIGHT,
+
+		SDL_WINDOW_SHOWN);
+	if(!window) {
+		return;
+	}
+
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		return;
+	}
+
+	playerpaddle.x = 20;
+	playerpaddle.y = 250;
+	playerpaddle.h = 100;
+	playerpaddle.w = 20;
+
+	AIpaddle.x = 760;
+	AIpaddle.y = 250;
+	AIpaddle.h = 100;
+	AIpaddle.w = 20;
+
+	Ball.x = 370;
+	Ball.y = 290; 
+	Ball.w = 20;
+	Ball.h = 20;
+
+
+}
