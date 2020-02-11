ce8de47beb95be9d7a0da48b117f66c68d447009
Sean Legroulx [Student] <sean.legroulx@student.sl.on.ca>
2020-02-11 12:21:24 -0500

    Lab 5 start

---
+++ lab5/lab5-SeanL/Part3-DodgingGame/CApp_OnInit.cpp
@@ -0,0 +1,46 @@
+//==============================================================================
+#include "CApp.h"
+#include <stdlib.h>
+#include <time.h>
+//==============================================================================
+bool CApp::OnInit()
+{
+
+    int i;
+
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK)<0)
+    {
+        return false;
+    }
+    window = SDL_CreateWindow("Basic SDL 2.0 Scrolling",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window)
+        return false;
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer)
+        return false;
+
+    srand(time(NULL));
+
+    for (i = 0; i < NUM_SPRITES; ++i) {
+		allplayers[i].imagename = "snowflake.bmp";
+        allplayers[i].loadPlayer(renderer);
+        allplayers[i].position.x = rand() % (window_w);
+        allplayers[i].position.y = rand() % (window_h);
+        allplayers[i].velocity.x = 0;
+        allplayers[i].velocity.y = 0;
+        while (!allplayers[i].velocity.x && !allplayers[i].velocity.y) {
+            allplayers[i].velocity.x = (rand() % (MAX_SPEED * 2 + 1)) - MAX_SPEED;
+            allplayers[i].velocity.y = (rand() % (MAX_SPEED * 2 + 1)) - MAX_SPEED;
+        }
+    }
+    
+    return true;
+}
+
+
+
+
+//==============================================================================
